a6aac50cc569a5c5488bff5449a60e51f02b73a9
Natalia Buzhovych <tuzhay@gmail.com>
2022-07-14 12:48:03 +0300

    HomeWork-14

---
+++ HomeWork-14/js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+console.log('test');
+$(document).ready(function () {
+           $('.one-time').slick({
+                dots: true,
+                infinite: true,
+                speed: 300,
+                slidesToShow: 1,
+                adaptiveHeight: true
+        });
+			
+});
