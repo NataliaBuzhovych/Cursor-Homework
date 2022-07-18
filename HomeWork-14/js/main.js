4f58caf2e8ed45c12b9a441fa6d9da357709b87f
Natalia Buzhovych <tuzhay@gmail.com>
2022-07-18 19:59:05 +0300

    delete js

--- HomeWork-14/js/main.js
+++ HomeWork-14/js/main.js
@@ -1,20 +1,20 @@
-  
-        $(document).ready(function () {
-                $('.one-time').slick({
-                    dots: true,
-                    infinite: true,
-                    speed: 300,
-                    slidesToShow: 1,
-                    adaptiveHeight: true
-            });
-                
-        });
 
-        // 2. This code loads the IFrame Player API code asynchronously.
-        var tag = document.createElement('script');
-        tag.src = "https://www.youtube.com/iframe_api";
-        var firstScriptTag = document.getElementsByTagName('script')[0];
-        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+$(document).ready(function () {
+    $('.one-time').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+
+});
+
+        // // 2. This code loads the IFrame Player API code asynchronously.
+        // var tag = document.createElement('script');
+        // tag.src = "https://www.youtube.com/iframe_api";
+        // var firstScriptTag = document.getElementsByTagName('script')[0];
+        // firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
         // 3. This function creates an <iframe> (and YouTube player)
         //    after the API code downloads.
